8ee8bd2fd7112c673fb40e253c6c371220d5a3d5
HARSHDEEP SINGH <harshdeepkv@gmail.com>
2024-12-04 16:56:29 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Events from "./pages/Events";
+import Contact from "./pages/Contact";
+import Donate from "./pages/Donate";
+import SignUp from "./pages/SignUp";
+import Admin from "./pages/Admin";
+import { useEffect } from "react"
+
+export default function App() {
+  useEffect(() => {
+    document.title = "Nexus"
+  }, [])
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/event" element={<Events />} />
+        <Route exact path="/contact" element={<Contact />} />
+        <Route exact path="/donate" element={<Donate />} />
+        <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/admin" element={<Admin />} />
+        <Route exact path="*" element={<Navigate to={"/"} />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
